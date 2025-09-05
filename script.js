376997811f1e2986687371b985a78d714f855b02
NoraxMine <noraxmine@tutamail.com>
2025-09-05 15:47:02 +0300

    "red"

---
+++ script.js
@@ -0,0 +1,120 @@
+//kjiohjiohji
+
+/*
+bhbhjbj
+knlklj
+kjkj
+hkhhohiohi
+// */
+
+// console.log(Infinity / Infinity);
+
+// console.log("rfesjfewjewjkfew"); 
+
+// console.log(((8/2) + 5) - (-3/2));
+
+// console.log("1"/1);
+
+// console.log("\\ \\ \\\\ \\\ \'\"")
+
+// console.log("fekj'dkj \"dfekji\" \n dewjn")
+
+
+// let word = "dwi"
+// let num = 10
+// let assaHi = "Hi it is дела"
+
+// window.alert("Hello world!");
+
+// console.log(word)
+// console.log(num)
+// console.log(assaHi) 
+
+
+
+
+// let b = "dkjkj"
+// let strB = b.length - 1;
+
+// console.log(b[b.length - 1]);
+
+
+// let c = gdg * 1;
+// // let strC = c.length - 1
+
+// console.log(typeof(c));
+
+
+
+// let word = "hello";
+
+// let fL = word[0].toUpperCase()
+
+// let nW = word.slice(1)
+
+// console.log(fL + nW)
+
+
+
+
+// let student = 'student'
+// let name = 'Mark'
+
+// console.log("This is a " + student + ". His name is " + name)
+
+// console.log(`This is a ${student}. His name is ${name}`)
+
+
+
+// let originalString = "Hello World";
+// let newString = originalString.slice(1, -1); // Extracts characters from index 0 up to (but not including) index 5
+// console.log(newString);
+
+
+
+// const text = '   Это текст, из которого мы хотим удалить      все пробелы .     ';
+// console.log(text.replaceAll(' ', '')); // => Этотекст,изкоторогомыхотимудалитьвсепробелы
+
+
+// let str = "ewkilewif joie efwji"
+// let num = 5
+
+// console.log(str.repeat(num))
+
+
+// let number = 1501
+
+// console.log(Math.ceil(number/100))
+
+// function century(year) {
+//     // Finish this :)
+//     ye = year % 100
+//     if (ye == 0) 
+//     {
+//         return year/100;
+
+//     }
+//     else{
+//       return Math.ceil(year / 100 ) ; 
+//     }
+    
+    
+    
+//   }
+
+//   let res = century(1809);
+
+//   console.log(res)
+
+function finalGrade (exam, projects) {
+  if (exam > 90 || projects > 10)
+    return console.log(100)
+  else if (exam > 75 && projects >= 5)
+    return console.log(75)
+  else if (exam > 50 && projects >= 2)
+    return console.log(50)
+  else 
+    return console.log(0)
+}
+
+finalGrade(85, 5)
